ef1d29138fe212d26cfa9c13a50eb8eb8794af31
Xiyu Zhai <zxy4970@gmail.com>
2020-02-03 17:46:29 -0800

    improve some functions

--- xiyu-unrollavx-365.c
+++ xiyu-unrollavx-365.c
@@ -1,9 +1,5 @@
-#include <x86intrin.h>
-#include <stdio.h>
 #include <immintrin.h>
-
-#include <xmmintrin.h>
-#include <emmintrin.h>
+#include <stdio.h>
 const char *dgemm_desc = "Simple blocked dgemm.";
 #pragma GCC optimize(3,"Ofast","inline","unroll-loops","prefetch-loop-arrays")
 
@@ -14,7 +10,7 @@ const char *dgemm_desc = "Simple blocked dgemm.";
 #define Mat(A,i,j) (A)[(j)*lda + (i)]
 #define min(a,b) (((a)<(b))?(a):(b))
 
-static inline void matmul_4xkxkx4(int lda, int K, double* a, double* b, double* restrict c,int rM,int rN)
+static inline void do_block_avx(int lda, int K, double* a, double* b, double* restrict c,int rM,int rN)
 {
   __m256d a_coli, bi0, bi1, bi2, bi3;
   __m256d c_col0,c_col1,c_col2,c_col3;
@@ -57,12 +53,12 @@ static inline void matmul_4xkxkx4(int lda, int K, double* a, double* b, double* 
     _mm256_storeu_pd(ctem+12, c_col3);
     for (int i=0;i<rM;i++){
       for (int j=0;j<rN;j++){
-      	c[i+j*lda]=ctem[i+j*4];
+      	c[i + j * lda]=ctem[i + j * 4];
       }
     }
   }
 }
-static inline void copy_a (int lda, const int K, double* a_src, double* a_dst) {
+static inline void copyA (int lda, const int K, double* a_src, double* a_dst) {
   for (int i = 0; i < K; ++i) 
   {
     for ( int j = 0; j < 4; ++j){
@@ -71,10 +67,10 @@ static inline void copy_a (int lda, const int K, double* a_src, double* a_dst) {
     a_src += lda;
   }
 }
-static inline void copyadd0_a (int lda, const int K, double* a_src, double* a_dst,int rc) {
+static inline void copyA_pad (int lda, const int K, double* a_src, double* a_dst,int rc) {
   for (int i = 0; i < K; ++i) 
   {
-    for ( int j = 0; j < 4; ++j){
+    for (int j = 0; j < 4; ++j){
       if (j>=rc){
         *a_dst=0;
 	      a_dst++;
@@ -88,7 +84,7 @@ static inline void copyadd0_a (int lda, const int K, double* a_src, double* a_ds
   }
 }
 // copy and transpose B
-static inline void copy_b (int lda, const int K, double* b_src, double* b_dst) {
+static inline void copyB (int lda, const int K, double* b_src, double* b_dst) {
   double* b_ptr[4];
   b_ptr[0] = b_src;
   for(int i = 1; i < 4; ++i){
@@ -101,7 +97,7 @@ static inline void copy_b (int lda, const int K, double* b_src, double* b_dst) {
     }
   }
 }
-static inline void copyadd0_b (int lda, const int K, double* b_src, double* b_dst,int rc) {
+static inline void copyB_pad (int lda, const int K, double* b_src, double* b_dst,int rc) {
   double* b_ptr[4];
   b_ptr[0] = b_src;
   for(int i = 1; i < rc; ++i){
@@ -124,26 +120,26 @@ static inline void copyadd0_b (int lda, const int K, double* b_src, double* b_ds
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static inline void do_block (int lda, int M, int N, int K,int newM,int newN, double* A, double* B, double* restrict C, double* A_block, double* B_block)
+static inline void my_do_block (int lda, int M, int N, int K,int newM,int newN, double* A, double* B, double* restrict C, double* A_block, double* B_block)
 {
   double *a_ptr, *b_ptr;
   /* For each column of B */
   for (int j = 0 ; j < newN; j += 4) 
   {
     b_ptr = &B_block[K * j];
-    if (j>N-4) {copyadd0_b(lda,K,B+j*lda,b_ptr,N-j);}
+    if (j>N-4) {copyB_pad(lda,K,B+j*lda,b_ptr,N-j);}
     else{// copy and transpose B_block
-    copy_b(lda, K, B + j*lda, b_ptr);}
+    copyB(lda, K, B + j*lda, b_ptr);}
 
     for (int i = 0; i < newM; i += 4) {
       a_ptr = &A_block[i * K];
       if (j == 0){
-        if (i>M-4){copyadd0_a(lda,K,A+i,a_ptr,M-i);}
-        else{copy_a(lda, K, A + i, a_ptr);}
+        if (i>M-4){copyA_pad(lda,K,A+i,a_ptr,M-i);}
+        else{copyA(lda, K, A + i, a_ptr);}
       }
       int rM=min(4,M-i);
       int rN=min(4,N-j);
-      matmul_4xkxkx4(lda, K, a_ptr, b_ptr, C+i+j*lda,rM,rN);
+      do_block_avx(lda, K, a_ptr, b_ptr, C+i+j*lda,rM,rN);
     }
   }
 }
@@ -163,33 +159,26 @@ static inline void do_block (int lda, int M, int N, int K,int newM,int newN, dou
   // reorder loops for cache efficiency
   for (int k1 = 0; k1 < lda; k1 += BLOCK_L2K) 
   {
-    // For each block column of B 
     for (int j1 = 0; j1 < lda; j1 += BLOCK_L2N) 
     {
-      // For each block row of A 
       for (int i1 = 0; i1 < lda; i1 += BLOCK_L2M) 
       {
-        // compute end index of smaller block
         int end_k = k1 + min(BLOCK_L2K, lda-k1);
         int end_j = j1 + min(BLOCK_L2N, lda-j1);
         int end_i = i1 + min(BLOCK_L2M, lda-i1);
-
         for (int k = k1; k < end_k; k += BLOCK_L1K) 
         {
-          // For each block column of B 
           for (int j = j1; j < end_j; j += BLOCK_L1N) 
           {
-            // For each block row of A
             for (int i = i1; i < end_i; i += BLOCK_L1M) 
             {
-              // compute block size
               int K = min(BLOCK_L1K, end_k-k);
               int N = min(BLOCK_L1N, end_j-j);
               int M = min(BLOCK_L1M, end_i-i);
               /* Performs a smaller dgemm operation
                *  C' := C' + A' * B'
                * where C' is M-by-N, A' is M-by-K, and B' is K-by-N. */
-              //do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+              //my_do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
               int resrow = M % 4;
               int rescol = N % 4;
               int newM=M;
@@ -201,7 +190,7 @@ static inline void do_block (int lda, int M, int N, int K,int newM,int newN, dou
                 newN=N+4-rescol;
               }
 
-              do_block(lda, M, N, K, newM,newN,A + i + k*lda, B + k + j*lda, C + i + j*lda, A_block, B_block);
+              my_do_block(lda, M, N, K, newM,newN,A + i + k*lda, B + k + j*lda, C + i + j*lda, A_block, B_block);
             }
           }
         }
--- xiyu-unrollavx256-40.c
+++ xiyu-unrollavx256-40.c
@@ -4,39 +4,30 @@
 
 const char* dgemm_desc = "Blocked dgemm with padding and AVX.";
 
-#if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 64
-#define BLOCK_SIZE2 64
+#if !defined(BLOCK_SIZEk)
+#define BLOCK_SIZEk 64
+#define BLOCK_SIZEj 64
+#define BLOCK_SIZEi 32
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
-// declare the functions
-static void pad(double* restrict padA, double* restrict A, const int lda, const int newlda);
-static void unpad(double* restrict padA, double* restrict A, const int lda, const int newlda);
-void transposeA(double* restrict AT, double* restrict A, const int lda, const int ldb);
-void copyA(double* restrict AT, double* restrict A, const int lda);
-static void do_block (const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C);
-static void do_block_unrollavx128(const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C);
-static void do_block_unrollavx256(const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C);
-void square_dgemm (const int lda, double* restrict A, double* restrict B, double* restrict C);
-
 ///copy array into the padded matrix
-static void pad(double* restrict padA, double* restrict A, const int lda, const int newlda)
+static void pad(double* restrict padA, double* restrict A, const int lda, const int matsize)
 {
   for (int j = 0; j < lda; j++) {
     for (int i = 0; i < lda; i++) {
-      padA[i + j*newlda] = A[i + j*lda];
-    }   
-  }   
+      padA[i + j*matsize] = A[i + j*lda];
+    }
+  }
 }
 
 //copy array into the unpadded matrix
-static void unpad(double* restrict padA, double* restrict A, const int lda, const int newlda)
+static void unpad(double* restrict padA, double* restrict A, const int lda, const int matsize)
 {
   for (int j = 0; j < lda; j++) {
     for (int i = 0; i < lda; i++) {
-      A[i + j*lda] = padA[i + j*newlda];
+      A[i + j*lda] = padA[i + j*matsize];
     }   
   }   
 }
@@ -62,64 +53,13 @@ void copyA(double* restrict AT, double* restrict A, const int lda)
   }   
 }
 
-/* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C)
-{
-  /* For each row i of A */
-  for (int i = 0; i < M; ++i)
-  {
-    /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k){
-        cij += A[i+k*lda] * B[k+j*lda];
-        C[i+j*lda] = cij;
-      }
-    }
-  }
-}
-
-/* UNROLL 2: This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block_unrollavx128(const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C)
-{
-  __m128d Aik,Aik_1,Bkj,Bkj_1,Bkj_2,Bkj_3,Cij,Cij_1,d0,d1;
-
-  for (int k = 0; k < K; k += 2 ) {
-    for (int j = 0; j < N; j += 2) {
-      Bkj = _mm_load1_pd(B+k+j*lda);
-      Bkj_1 = _mm_load1_pd(B+k+1+j*lda);
-      Bkj_2 = _mm_load1_pd(B+k+(j+1)*lda);
-      Bkj_3 = _mm_load1_pd(B+k+1+(j+1)*lda);
-      for (int i = 0; i < M; i += 2) {
-        Aik = _mm_load_pd(A+i+k*lda);
-        Aik_1 = _mm_load_pd(A+i+(k+1)*lda);
-
-        Cij = _mm_load_pd(C+i+j*lda);
-        Cij_1 = _mm_load_pd(C+i+(j+1)*lda);
-
-        d0 = _mm_add_pd(Cij, _mm_mul_pd(Aik,Bkj));
-        d1 = _mm_add_pd(Cij_1, _mm_mul_pd(Aik,Bkj_2));
-        Cij = _mm_add_pd(d0, _mm_mul_pd(Aik_1,Bkj_1));
-        Cij_1 = _mm_add_pd(d1, _mm_mul_pd(Aik_1,Bkj_3));
-        _mm_store_pd(C+i+j*lda,Cij);
-        _mm_store_pd(C+i+(j+1)*lda,Cij_1);
-      }
-    }
-  }
-}
-
 /* UNROLL 2: This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block_unrollavx256(const int lda, const int M, const int N, const int K, double* restrict A, double* restrict B, double* restrict C)
 {
-  __m256d Aik, Aik_1, Bkj, Bkj_1, Bkj_2, Bkj_3, Cij, Cij_1, Cij_2, Cij_3, Cij_4, Cij_5, Cij_6, Cij_7;
+  __m256d Aik, Aik_1, Bkj, Bkj_1, Bkj_2, Bkj_3;
+  __m256d Cij, Cij_1, Cij_2, Cij_3, Cij_4, Cij_5, Cij_6, Cij_7;
 
   for (int i = 0; i < M; i += 8) {
     for (int j = 0; j < N; j += 4) {
@@ -172,51 +112,35 @@ static void do_block_unrollavx256(const int lda, const int M, const int N, const
  * On exit, A and B maintain their input values. */  
 void square_dgemm (const int lda, double* restrict A, double* restrict B, double* restrict C)
 {
+    int matsize = lda;
+    int div = 8;
+    int rem = lda % div;
+    if (rem){
+        matsize = lda + (div-rem) + div;
+    }
 
-  // MODE = 0 -> use 128avx and unroll
-  // MODE = 1 -> use 256avx and unroll
-  int MODE = 1;
+    double* padA = (double*) _mm_malloc(matsize * matsize * sizeof(double), 32);
+    double* padB = (double*) _mm_malloc(matsize * matsize * sizeof(double), 32);
+    double* padC = (double*) _mm_malloc(matsize * matsize * sizeof(double), 32);
+    pad(padA, A, lda, matsize);
+    pad(padB, B, lda, matsize);
+    pad(padC, C, lda, matsize);
 
-  int newlda = lda;
-  int div = 8;
-  if (lda % div){
-    int t = lda % div;
-    newlda = lda + (div-t) + div;
-  }
-
-  double* padA = (double*) _mm_malloc(newlda * newlda * sizeof(double), 32);
-  pad(padA, A, lda, newlda);
-
-  double* padB = (double*) _mm_malloc(newlda * newlda * sizeof(double), 32);
-  pad(padB, B, lda, newlda);
-
-  double* padC = (double*) _mm_malloc(newlda * newlda * sizeof(double), 32);
-  pad(padC, C, lda, newlda);
-
-  /* For each block-row of A */ 
-  for (int i = 0; i < newlda; i += BLOCK_SIZE2)
-  {
-    /* For each block-column of B */
-    for (int j = 0; j < newlda; j += BLOCK_SIZE)
+    for (int k = 0; k < matsize; k += BLOCK_SIZEk)
     {
-      /* Accumulate block dgemms into block of C */
-      for (int k = 0; k < newlda; k += BLOCK_SIZE)
+      for (int j = 0; j < matsize; j += BLOCK_SIZEj)
       {
-        /* Correct block dimensions if block "goes off edge of" the matrix */
-        int M = min (BLOCK_SIZE2, newlda-i);
-        int N = min (BLOCK_SIZE, newlda-j);
-        int K = min (BLOCK_SIZE, newlda-k);
-        /* Perform individual block dgemm */
-
-        if (MODE == 0) do_block_unrollavx128(newlda, M, N, K, padA + i + k*newlda, padB + k + j*newlda, padC + i + j*newlda);
-        else do_block_unrollavx256(newlda, M, N, K, padA + i + k*newlda, padB + k + j*newlda, padC + i + j*newlda);
-
+        for (int i = 0; i < matsize; i += BLOCK_SIZEi)
+        {
+          int M = min (BLOCK_SIZEi, matsize-i);
+          int N = min (BLOCK_SIZEj, matsize-j);
+          int K = min (BLOCK_SIZEk, matsize-k);
+          do_block_unrollavx256(matsize, M, N, K, padA + i + k*matsize, padB + k + j*matsize, padC + i + j*matsize);
+        }
       }
     }
-  }
 
-  unpad(padC, C, lda, newlda);
-  _mm_free(padA);
-  _mm_free(padB);
-
+    unpad(padC, C, lda, matsize);
+    _mm_free(padA);
+    _mm_free(padB);
 }
